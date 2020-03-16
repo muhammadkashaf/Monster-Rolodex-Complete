110096566f9452b8e8c26da76f21025645be41df
Muhammad <kashafahmed72@gmail.com>
2020-03-16 10:07:40 +0500

    Updates

---
+++ precache-manifest.d76281853b864ea9bddec9f0e0410e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b04537184c68616b2b80f8522aab9be",
+    "url": "/Monster-Rolodex-Complete/index.html"
+  },
+  {
+    "revision": "249e0e82a08c050ed1bb",
+    "url": "/Monster-Rolodex-Complete/static/css/main.c50b4411.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/Monster-Rolodex-Complete/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodex-Complete/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249e0e82a08c050ed1bb",
+    "url": "/Monster-Rolodex-Complete/static/js/main.c210f6be.chunk.js"
+  },
+  {
+    "revision": "17bf2dee6f87ecfa93c6",
+    "url": "/Monster-Rolodex-Complete/static/js/runtime-main.cfd1054b.js"
+  }
+]);
